ca9c5881ce6d5882cb382c27e814b71928b50aa9
roninen <osoite@eitoimi.fi>
2018-04-17 15:58:06 +0300

    Hrm. File management error

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,14 @@ if [ "$TRAVIS_PULL_REQUEST" == true ]; then
     exit 0
 fi
 
-export REPO="roninen/kerrokantasi-ui"
+export REPO="roninen/hello-world"
 
 # Escape slashes in branch names
 export BRANCH=${TRAVIS_BRANCH//\//_}
 
 if [ "$TRAVIS_BRANCH" == "master" ] ; then
     echo "Tagging master"
-    docker tag kerrokantasi-ui "$REPO:$COMMIT"
+    docker tag hello-world "$REPO:$COMMIT"
     docker tag "$REPO:$COMMIT" $REPO:latest
     docker push "$REPO:$COMMIT"
     docker push "$REPO:latest"
@@ -21,7 +21,7 @@ fi
 
 if [ "$TRAVIS_BRANCH" != "master" ] ; then
     echo "Tagging branch " "$TRAVIS_BRANCH"
-    docker tag kerrokantasi-ui "$REPO:$COMMIT"
+    docker tag hello-world "$REPO:$COMMIT"
     docker tag "$REPO:$COMMIT" "$REPO:$BRANCH"
     docker tag "$REPO:$COMMIT" "$REPO:travis-$TRAVIS_BUILD_NUMBER"
     docker push "$REPO:$COMMIT"
